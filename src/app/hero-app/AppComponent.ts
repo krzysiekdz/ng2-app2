ddd0ecd446fef88742d2ac72c073b17236b48774
unknown <krzysztof.dziezyk@gmail.com>
2017-10-03 08:35:31 +0200

    initial

---
+++ src/app/hero-app/AppComponent.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+	templateUrl: './app.component.html',
+	selector: '',
+})
+export class AppComponent {
+	title = 'hero app';
+	hero = 'Spiderman';
+}
